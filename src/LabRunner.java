696f3cc0888f220b89351f06ead17c14153720e3
Kent Li <kentl5@nycstudents.net>
2022-10-10 14:50:13 -0400

    Finish

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -1,11 +1,14 @@
 public class LabRunner {
     public static void main (String[] args) {
-        System.out.println("    /\\\n" +
-                "   /\"\"\\\n" +
-                "  /\"  \"\\\n" +
-                "  \\\"  \"/\n" +
-                "   \\\"\"/\n" +
-                "    \\/\n");
+//        System.out.println("Nice to see you!");
+//        System.out.println("Goodbye!");
+//        System.out.print("Nice to see you!\n");
+//        System.out.println("Goodbye!");
+        Student student = new Student("Abby", "Smith", 2023);
+        student.addTestScore(95);
+        student.addTestScore(98);
+        System.out.println(student.getStudentInfo());
+
 
     }
 }
